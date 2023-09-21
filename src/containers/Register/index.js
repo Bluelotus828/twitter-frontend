c4f92ae612a50a3ba9cd990e27d470ec59cb538f
Bluelotus828 <yuguanlin828@gmail.com>
2023-09-20 21:49:21 -0700

    feature/register-page with icon

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -6,27 +6,27 @@ import style from './index.module.css';
 /**
  * reigister page
  */
-const Register  = () => {
+const Register = () => {
   console.log('>>>');
   return (
-  <div>
-    <div className={style.header}>
-      <CloseOutline className={style.closeIcon} />
-      <img src= {logo} alt="twitter-logo" className={style.twitterLogo} />
-    </div>
-    <div className="form">
-      <div>Create Your Account</div>
-      <Input placeholder="Account Name" />
-      <Input placeholder="Phone Number" />
-      <div>Use Email Address</div>
-      <div>Date of Birth</div>
-      <div>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</div>
-      <Input placeholder="" />
-    </div>
-    <div className="footer"></div>
+    <div>
+      <div className={style.header}>
+        <CloseOutline className={style.closeIcon} />
+        <img src={logo} alt="twitter-logo" className={style.twitterLogo} />
+      </div>
+      <div className="form">
+        <div>Create Your Account</div>
+        <Input placeholder="Account Name" />
+        <Input placeholder="Phone Number" />
+        <div>Use Email Address</div>
+        <div>Date of Birth</div>
+        <div>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</div>
+        <Input placeholder="" />
+      </div>
+      <div className="footer" />
       <Button>Next Step</Button>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
